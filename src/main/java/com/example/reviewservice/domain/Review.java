5fbe7dd5c71f0aa92733e55ff284bb86e50cadc3
kimsegwang <jjjkk24@gmail.com>
2025-01-01 16:11:37 +0900

    review_insert

--- src/main/java/com/example/reviewservice/domain/Review.java
+++ src/main/java/com/example/reviewservice/domain/Review.java
@@ -3,13 +3,15 @@ package com.example.reviewservice.domain;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 public class Review {
     @Id
     private Long id;
@@ -21,14 +23,14 @@ public class Review {
     private String content;
 
     @Column("author_id")
-    private Integer authorId;
+    private String authorId;
 
     @CreatedDate
     @Column("created_at")
     private LocalDateTime createdAt;
 
     @Column("rating")
-    private Integer rating;
+    private double rating;
 
     @Column("img") // 여러 이미지 경로를 ';'로 연결
     private String img;
